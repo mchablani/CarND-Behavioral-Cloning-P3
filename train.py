e5c96bc9d43270ab52584a253026028f30f6a357
Manish Chablani <chablani@uber.com>
2017-04-05 20:54:50 -0700

    Model that works great on track 1 of simulator

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import cv2
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Lambda, Convolution2D, MaxPooling2D, Dropout, Activation, Cropping2D
+from keras.layers import Dense, Flatten, Lambda, Convolution2D, MaxPooling2D, Dropout, Activation, Cropping2D, BatchNormalization
 import numpy as np
 import keras as k
 
@@ -9,8 +9,11 @@ images = []
 measurements = []
 
 
-def getImages(imgPath, type, adj):
-  log = pd.read_csv(imgPath + '/driving_log.csv', header=None, names=['center', 'left', 'right', 'steering', 't', 'b', 's'])
+def getImages(imgPath, type, adj, header):
+  if header:
+    log = pd.read_csv(imgPath + '/driving_log.csv')
+  else:
+    log = pd.read_csv(imgPath + '/driving_log.csv', header=None, names=['center', 'left', 'right', 'steering', 't', 'b', 's'])
   # print(log.columns)
 
   images = []
@@ -24,7 +27,8 @@ def getImages(imgPath, type, adj):
     source_path = imgPath + "/IMG/" + fn
     # print(source_path)
     image = cv2.imread(source_path)
-    measurement = l['steering'] + adj
+    s = l['steering']
+    measurement = float(s) + adj
     images.append(image)
     measurements.append(measurement)
 
@@ -34,24 +38,63 @@ def getImages(imgPath, type, adj):
     images.append(image_flipped)
     measurements.append(measurement_flipped)
 
-  print(len(images))
-  print(len(measurements))
+  # print(len(images))
+  # print(len(measurements))
   return images, measurements
   # image_flipped = np.fliplr(images)
   # measurement_flipped = -measurement
 
 
-# print(images[2].shape)
+def getImagesForDir(dir, header=False):
+  images = []
+  measurements = []
+  images, measurements = getImages(dir, 'center', 0.0, header)
+  imagesl, measurementsl = getImages(dir, 'left', 1.5, header)
+  imagesr, measurementsr = getImages(dir, 'right', -1.5, header)
 
-images, measurements = getImages("./data", 'center', 0)
-imagesl, measurementsl = getImages("./data", 'left', 0.2)
-imagesr, measurementsr = getImages("./data", 'right', -0.2)
+  images.extend(imagesl)
+  images.extend(imagesr)
 
-images.extend(imagesl)
-images.extend(imagesr)
+  measurements.extend(measurementsl)
+  measurements.extend(measurementsr)
+  return images, measurements
 
-measurements.extend(measurementsl)
-measurements.extend(measurementsr)
+
+# i, m = getImagesForDir("./data")
+# images.extend(i)
+# measurements.extend(m)
+# print(len(images))
+
+i, m = getImagesForDir("./data.bk", header=True)
+images.extend(i)
+measurements.extend(m)
+print(len(images))
+
+# i, m = getImagesForDir("./data.2")
+# images.extend(i)
+# measurements.extend(m)
+# print(len(images))
+#
+# i, m = getImagesForDir("./data_1_r")
+# images.extend(i)
+# measurements.extend(m)
+# print(len(images))
+
+# i, m = getImagesForDir("./data_2.1")
+# images.extend(i)
+# measurements.extend(m)
+# print(len(images))
+
+# i, m = getImagesForDir("./data_1_r.1")
+# images.extend(i)
+# measurements.extend(m)
+# print(len(images))
+
+i, m = getImagesForDir("./ndata_1_f.1")
+images.extend(i)
+measurements.extend(m)
+print(len(images))
+
 
 print(len(images))
 print(len(measurements))
@@ -59,23 +102,48 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 
+# model = Sequential()
+# model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+# model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+# model.add(Convolution2D(32, 3, 3))
+# model.add(MaxPooling2D((2, 2)))
+# model.add(Dropout(0.5))
+# model.add(Activation('relu'))
+# model.add(Convolution2D(128, 3, 3))
+# model.add(MaxPooling2D((2, 2)))
+# model.add(Dropout(0.5))
+# model.add(Activation('relu'))
+# model.add(Flatten())
+# model.add(Dense(128))
+# model.add(Dropout(0.5))
+# model.add(Activation('relu'))
+# model.add(Dense(1))
+
 model = Sequential()
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
-model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-model.add(Convolution2D(32, 3, 3))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.5))
-model.add(Activation('relu'))
-model.add(Convolution2D(128, 3, 3))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.5))
-model.add(Activation('relu'))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(BatchNormalization())
+model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.3))
+model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Dropout(0.3))
+model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Dropout(0.3))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Dropout(0.3))
 model.add(Flatten())
-model.add(Dense(128))
+model.add(Dense(100))
 model.add(Dropout(0.5))
-model.add(Activation('relu'))
+model.add(Dense(50))
+model.add(Dropout(0.5))
+model.add(Dense(10))
+model.add(Dropout(0.5))
 model.add(Dense(1))
 
+
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7, verbose=2, batch_size=128)
 model.save('model.h5')
